76826ea8190acc4791e8b0e6a51c3fd8d5e755c1
feifan-li <feifanl@usc.edu>
2024-07-20 16:03:14 +0800

    lc1605

--- Greedy/LC1605FindValidMatrixGivenRowAndColumnSums/Solution1605.cpp
+++ Greedy/LC1605FindValidMatrixGivenRowAndColumnSums/Solution1605.cpp
@@ -1,4 +1,9 @@
-class Solution {
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution1605 {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         // Greedy
@@ -18,3 +23,17 @@ public:
         return mat;
     }
 };
+
+int main(int argc, char* argv[]){
+  Solution1605 s;
+  vector<int> rowSum = {5, 7, 10};
+  vector<int> colSum = {8, 6, 8};
+  vector<vector<int>> ans = s.restoreMatrix(rowSum, colSum);
+  for(int i=0; i<ans.size(); ++i){
+    for(int j=0; j<ans[0].size(); ++j){
+      cout << ans[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
